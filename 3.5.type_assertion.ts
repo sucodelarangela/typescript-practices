c3f5a55c9e6360a9c2fdf86f5625b8731ccca7e9
Angela Caldas <aes.caldas@gmail.com>
2022-10-24 16:48:44 -0300

    chapter 2: type assertion

--- 3.5.type_assertion.ts
+++ 3.5.type_assertion.ts
@@ -1,6 +1,6 @@
 // Com o type assertion podemos mudar o type de uma variável. Basta passar o type entre <>
 function typeAssertions(codigoAny: any) {
-    let codigoNumber = <number>codigoAny;
+    let codigoNumber = <number>codigoAny; // aqui mudamos o parâmetro recebido na função de any para number
     return codigoAny
 }
 typeAssertions(10)
