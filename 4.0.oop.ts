d078baf413da8ed43bf8dc6e25d636dd0e735dc9
Angela Caldas <aes.caldas@gmail.com>
2022-10-28 12:51:55 -0300

    chapter 4: heritage

--- 4.0.oop.ts
+++ 4.0.oop.ts
@@ -3,15 +3,15 @@ class Conta {
     // a) atributos: armazenam as informações que descrevem o objeto (como variáveis)
     numeroDaConta: number;
     titular: string;
-    saldo: number;
+    private saldo: number; // private não permite que o atributo seja acessado fora da classe
 
     // c) Métodos: servem para identificar e executar operações que a classe fornecerá (manipular atributos)
     consultaSaldo(): string {
         return `O seu saldo atual é: ${this.saldo}`
     }
 
-    adicionaSaldo(saldo: number): void {
-        this.saldo + saldo
+    adicionaSaldo(valor: number): void {
+        this.saldo += valor
     }
 
     sacarDoSaldo(valor: number): void {
@@ -29,4 +29,28 @@ class Conta {
 
 // Para criar um objeto a partir da nossa classe, precisamos instancia-la através da palavra reservada NEW
 const primeiraConta = new Conta(1, "Thiago Adriano", 1000)
-console.log(primeiraConta.saldo)
+primeiraConta.adicionaSaldo(750)
+
+// Herança: permite reutilizar código sem a necessidade de duplica-lo (palavra chave EXTENDS)
+class contaPF extends Conta {
+    cpf: number;
+
+    constructor(cpf: number, numeroDaConta: number, titular: string, saldo: number) {
+        super(numeroDaConta, titular, saldo)
+        this.cpf = cpf
+    }
+}
+
+class contaPJ extends Conta {
+    cnpj: number;
+
+    constructor(cnpj: number, numeroDaConta: number, titular: string, saldo: number) {
+        super(numeroDaConta, titular, saldo)
+        this.cnpj = cnpj
+    }
+}
+
+const pessoaFisica = new contaPF(12345678900, 1, "Thiago Adriano", 1000)
+const pessoaJuridica = new contaPJ(12345678000177, 1, "Thiago Adriano", 1000)
+
+console.log(pessoaJuridica)
