e62bfc69e6437ea6215178a9a1d471b001b77e83
Angela Caldas <aes.caldas@gmail.com>
2022-10-29 13:32:54 -0300

    chapter 4: protected methods

--- 4.0.oop.ts
+++ 4.0.oop.ts
@@ -1,42 +1,52 @@
 // Como boa prática, o objeto conta ficaria dentro de um arquivo com o mesmo nome dela, por exemplo "conta.ts"
 class Conta {
     // a) atributos: armazenam as informações que descrevem o objeto (como variáveis)
-    numeroDaConta: number;
+    private numeroDaConta: number;
     titular: string;
     private saldo: number; // private não permite que o atributo seja acessado fora da classe
 
     // c) Métodos: servem para identificar e executar operações que a classe fornecerá (manipular atributos)
-    consultaSaldo(): string {
-        return `O seu saldo atual é: ${this.saldo}`
+    // protected faz com que os métodos sejam acessados somente pela própria classe Conta e pelas classes que a herdarem
+    protected consultaSaldo(): number {
+        return this.saldo
     }
 
-    adicionaSaldo(valor: number): void {
+    protected adicionaSaldo(valor: number): void {
         this.saldo += valor
     }
 
-    sacarDoSaldo(valor: number): void {
+    protected sacarDoSaldo(valor: number): void {
         this.saldo -= valor
     }
 
     // b) construtor: espécie de função para acessar os atributos
-    constructor(numeroDaConta: number, titular: string, saldo: number) {
-        this.numeroDaConta = numeroDaConta;
+    constructor(titular: string, saldo: number) {
+        this.numeroDaConta = Math.floor(Math.random() * 1000) + 1;
         this.titular = titular;
         this.saldo = saldo;
     }
-
 }
 
 // Para criar um objeto a partir da nossa classe, precisamos instancia-la através da palavra reservada NEW
-const primeiraConta = new Conta(1, "Thiago Adriano", 1000)
-primeiraConta.adicionaSaldo(750)
+const primeiraConta = new Conta("Thiago Adriano", 1000)
+primeiraConta.adicionaSaldo(750) // error por causa do protected
 
 // Herança: permite reutilizar código sem a necessidade de duplica-lo (palavra chave EXTENDS)
 class contaPF extends Conta {
     cpf: number;
 
-    constructor(cpf: number, numeroDaConta: number, titular: string, saldo: number) {
-        super(numeroDaConta, titular, saldo)
+    consultar(): string {
+        return `Saldo atual: ${this.consultaSaldo()}`
+    }
+
+    sacar(valor: number) {
+        if (this.consultaSaldo() > 0 && valor <= this.consultaSaldo()) {
+            this.sacarDoSaldo(valor)
+        }
+    }
+
+    constructor(cpf: number, titular: string, saldo: number) {
+        super(titular, saldo)
         this.cpf = cpf
     }
 }
@@ -44,13 +54,24 @@ class contaPF extends Conta {
 class contaPJ extends Conta {
     cnpj: number;
 
-    constructor(cnpj: number, numeroDaConta: number, titular: string, saldo: number) {
-        super(numeroDaConta, titular, saldo)
+    consultar(): string {
+        return `Saldo atual: ${this.consultaSaldo()}`
+    }
+
+    // na conta PJ o cliente pode ficar negativo
+    sacar(valor: number) {
+        this.sacarDoSaldo(valor)
+    }
+
+    constructor(cnpj: number, titular: string, saldo: number) {
+        super(titular, saldo)
         this.cnpj = cnpj
     }
 }
 
-const pessoaFisica = new contaPF(12345678900, 1, "Thiago Adriano", 1000)
-const pessoaJuridica = new contaPJ(12345678000177, 1, "Thiago Adriano", 1000)
+const pessoaFisica = new contaPF(12345678900, "Thiago Adriano", 1000)
+const pessoaJuridica = new contaPJ(12345678000177, "Thiago Adriano", 1000)
 
-console.log(pessoaJuridica)
+console.log(pessoaFisica.numeroDaConta)
+
+// Melhores práticas implementadas: número da conta gerado pela classe em vez de ser passado, método sacar com validação de saldo, métodos da classe pai protegidos para serem acessados somente nas classes filhas
