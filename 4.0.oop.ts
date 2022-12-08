8b974ef3b0bc0da14fd0ed61688263987ee2c992
Angela Caldas <aes.caldas@gmail.com>
2022-12-08 11:25:01 -0300

    chapter 8: namespaces

--- 4.0.oop.ts
+++ 4.0.oop.ts
@@ -1,90 +1,107 @@
 // Como boa prática, o objeto conta ficaria dentro de um arquivo com o mesmo nome dela, por exemplo "conta.ts"
-abstract class Conta {
-    // a) atributos: armazenam as informações que descrevem o objeto (como variáveis)
-    private readonly _numeroDaConta: number; // readonly impede alterações
-    titular: string;
-    private _saldo: number; // private não permite que o atributo seja acessado fora da classe
+// NAMESPACES
+namespace Banco {
+    export abstract class Conta {
+        // a) atributos: armazenam as informações que descrevem o objeto (como variáveis)
+        private readonly _numeroDaConta: number; // readonly impede alterações
+        titular: string;
+        private _saldo: number; // private não permite que o atributo seja acessado fora da classe
 
-    // d) Getter
-    get numeroDaConta(): number {
-        return this._numeroDaConta;
-    }
+        // d) Getter
+        get numeroDaConta(): number {
+            return this._numeroDaConta;
+        }
 
-    // c) Métodos: servem para identificar e executar operações que a classe fornecerá (manipular atributos)
-    // protected faz com que os métodos sejam acessados somente pela própria classe Conta e pelas classes que a herdarem
-    protected consultaSaldo(): number {
-        return this._saldo;
+        // c) Métodos: servem para identificar e executar operações que a classe fornecerá (manipular atributos)
+        // protected faz com que os métodos sejam acessados somente pela própria classe Conta e pelas classes que a herdarem
+        protected consultaSaldo(): number {
+            return this._saldo;
 
-    }
+        }
 
-    protected adicionaSaldo(@saldo() saldo: number): void {
-        this._saldo += saldo;
-    }
+        protected adicionaSaldo(@saldo() saldo: number): void {
+            this._saldo += saldo;
+        }
 
-    protected sacarDoSaldo(valor: number): void {
-        this._saldo -= valor;
-    };
+        protected sacarDoSaldo(valor: number): void {
+            this._saldo -= valor;
+        };
 
-    // b) construtor: espécie de função para acessar os atributos
-    constructor(titular: string, saldo: number) {
-        this._numeroDaConta = Math.floor(Math.random() * 1000) + 1;
-        this.titular = titular;
-        this._saldo = saldo;
+        // b) construtor: espécie de função para acessar os atributos
+        constructor(titular: string, saldo: number) {
+            this._numeroDaConta = Math.floor(Math.random() * 1000) + 1;
+            this.titular = titular;
+            this._saldo = saldo;
+        }
     }
 }
 
 // Para criar um objeto a partir da nossa classe, precisamos instancia-la através da palavra reservada NEW
-const primeiraConta = new Conta("Thiago Adriano", 1000); // error por ser classe abstrata
+const primeiraConta = new Banco.Conta("Thiago Adriano", 1000); // error por ser classe abstrata
 primeiraConta.adicionaSaldo(750); // error por causa do protected
 
 // Herança: permite reutilizar código sem a necessidade de duplica-lo (palavra chave EXTENDS)
-class ContaPF extends Conta implements Tributavel {
-    cpf: number;
 
-    consultar(): string {
-        return `Saldo atual: ${this.consultaSaldo()}`;
-    }
+// NAMESPACES: herança
+namespace Banco {
+    export class ContaPF extends Conta implements Tributavel {
+        cpf: number;
 
-    sacar(valor: number) {
-        if (this.consultaSaldo() > 0 && valor <= this.consultaSaldo()) {
-            this.sacarDoSaldo(valor);
+        consultar(): string {
+            return `Saldo atual: ${this.consultaSaldo()}`;
+        }
+
+        sacar(valor: number) {
+            if (this.consultaSaldo() > 0 && valor <= this.consultaSaldo()) {
+                this.sacarDoSaldo(valor);
+            }
+        }
+
+        CalculaTributo(): number {
+            // implementação
+        }
+
+        constructor(cpf: number, titular: string, saldo: number) {
+            super(titular, saldo);
+            this.cpf = cpf;
         }
     }
+}
 
-    CalculaTributo(): number {
-        // implementação
-    }
+namespace Banco {
+    export class ContaPJ extends Conta implements Tributavel {
+        cnpj: number;
 
-    constructor(cpf: number, titular: string, saldo: number) {
-        super(titular, saldo);
-        this.cpf = cpf;
+        consultar(): string {
+            return `Saldo atual: ${this.consultaSaldo()}`;
+        }
+
+        // na conta PJ o cliente pode ficar negativo
+        sacar(valor: number) {
+            this.sacarDoSaldo(valor);
+        }
+
+        CalculaTributo(): number {
+            // implementação
+        }
+
+        constructor(cnpj: number, titular: string, saldo: number) {
+            super(titular, saldo);
+            this.cnpj = cnpj;
+        }
     }
 }
 
-class ContaPJ extends Conta implements Tributavel {
-    cnpj: number;
-
-    consultar(): string {
-        return `Saldo atual: ${this.consultaSaldo()}`;
-    }
-
-    // na conta PJ o cliente pode ficar negativo
-    sacar(valor: number) {
-        this.sacarDoSaldo(valor);
-    }
-
-    CalculaTributo(): number {
-        // implementação
-    }
-
-    constructor(cnpj: number, titular: string, saldo: number) {
-        super(titular, saldo);
-        this.cnpj = cnpj;
-    }
+namespace Banco {
+    export class ContaSalario extends Conta { }
 }
 
-const pessoaFisica = new ContaPF(12345678900, "Thiago Adriano", 1000);
-const pessoaJuridica = new ContaPJ(12345678000177, "Thiago Adriano", 1000);
+namespace Banco {
+    export class ContaInvestimento extends Conta { }
+}
+
+const pessoaFisica = new Banco.ContaPF(12345678900, "Thiago Adriano", 1000);
+const pessoaJuridica = new Banco.ContaPJ(12345678000177, "Thiago Adriano", 1000);
 
 console.log(pessoaFisica.numeroDaConta); // sem o getter, retorn undefined
 
@@ -94,7 +111,7 @@ console.log(pessoaFisica.numeroDaConta); // sem o getter, retorn undefined
 // SETTER: método utilizado quando queremos alterar o valor de uma propriedade
 
 // Classes abstratas: não permitem realizar qualquer tipo de instância, elas são usadas como modelos pra outras classes (concretas). Basta adicionar a palavra chave 'abstract' antes da classe.
-const novaConta = new Conta(); // error Cannot create an instance of an abstract class.
+const novaConta = new Banco.Conta(); // error Cannot create an instance of an abstract class.
 
 // IMPLEMENTANDO INTERFACES
 // Interfaces funcionam como um "contrato" que obriga a classe a definir todos os seus métodos e propriedades
---
+++ 8.0.modules_namespaces.ts
@@ -0,0 +1,5 @@
+// NAMESPACES
+// Permite organizar o código, deixando-o agrupado por nome, por exemplo: imaginando que temos a demanda de criar mais duas implementações da classe Conta, uma para Salário e outra para Investimento. Poderíamos criar assim:
+// class ContaSalario extends Conta {}
+// class ContaInvestimento extends Conta {}
+// Porém, começamos a expandir demais nosso código. Para isso, podemos criar um namespace Banco para organizar nossas classes. Vamos implementar isso no nosso arquivo 4.0.oop.ts
