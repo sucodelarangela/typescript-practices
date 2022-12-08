6f535abd9598d320b2631b519b4c8d7fd204b02c
Angela Caldas <aes.caldas@gmail.com>
2022-12-08 11:52:16 -0300

    TS concepts folder

--- 1.ts_concepts/8.0.modules_namespaces.ts
+++ 1.ts_concepts/8.0.modules_namespaces.ts
@@ -8,3 +8,17 @@
 // Utilizam o mesmo conceito de namespaces com a utilização da palavra reservada export, mas precisamos usar um module loader e, para chamar um module dentro de outro arquivo, precisamos usar a palavra reservada import.
 // Module loader é uma biblioteca que nos permite trabalhar com modules nos nossos projetos. Os mais comuns são CommonJs e Require.js
 // Vamos alterar os exemplos anteriores de namespaces para modules.
+
+// QUANDO USAR:
+/**
+namespaces:
+ * Trabalham com escopo global, então não precisam de import/export
+ * Organização mais lógica e feita em objetos
+ * Um namespace pode ser usado em mais de um arquivo
+ * Não precisam de nenhuma biblioteca para serem usados
+
+modules:
+* Não ficam em escopo global, então precisam de import/export
+* Cada implementação é um module separado
+* Precisam de uma biblioteca
+*/
